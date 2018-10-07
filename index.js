a8ee625a5f0bd4ed3b37eca6ae8a517e031a2673
abstract-animation-9914 <vm930@hotmail.com>
2018-10-07 15:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+//destructivelyAppendDriver(name)
+function destructivelyAppendDriver(name){
+  drivers.push(name);  
+}
+//destructivelyPrependDriver(name)
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+//destructivelyRemoveLastDriver(name)
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+//destructivelyRemoveFirstDriver()
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+//appendDriver(name)
+function appendDriver(name){
+  const _name_ = [...drivers, name]
+}
+
+//prependDriver(name)
+function prependDriver(name){
+  const copysOfDrivers = [...drivers, "name"];
+  return copysOfDrivers;
+}
+
+//removeLastDriver()
+function removeLastDriver(){
+  const removeLastDriver = drivers.splice(-1)
+}
